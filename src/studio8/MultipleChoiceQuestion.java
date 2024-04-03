d060ca4aec940bcaf6065c623a4eb7f5ff6af2be
emersonstein <emersonstein@localhost>
2024-04-03 14:11:11 -0500

    Code

--- src/studio8/MultipleChoiceQuestion.java
+++ src/studio8/MultipleChoiceQuestion.java
@@ -1,35 +1,32 @@
 package studio8;
 
 public class MultipleChoiceQuestion extends Question {
+	private String[] choices;
 	
 	public MultipleChoiceQuestion(String prompt, String answer, int points, String[] choices) {
-		//FIXME
-		
-		//Call the super class constructor, then create and set
-		//instance variables for any values that aren't handled
-		//by the base class
+		super(prompt, answer, points);
+			this.choices = choices; 
 	}
 	
 	public void displayPrompt() {
-		//FIXME
-		
-		/*
-		 * Use the method from the base class to display the prompt,
-		 * Then write more code to display all of the choices like so:
-		 * 
-		 * 1. Choice1
-		 * 2. Choice2
-		 * 3. Choice3
-		 * ...
-		 * N. ChoiceN
-		 */
-		//
+		System.out.println(super.getPrompt() + "(" + this.getPoints() + " points)");
+		int count = 0;
+		for (int i=0; i<choices.length; i++) {
+			count++;
+			System.out.println(count + ". " + choices[i]);
+		}
 	}
 	
 	public static void main(String[] args) {
 		String[] choices = {"seven", "nine", "eight", "six"};
 		Question multipleChoice = new MultipleChoiceQuestion("What studio is this?", "3", 1, choices);
 		multipleChoice.displayPrompt();
+		System.out.println(multipleChoice.checkAnswer("hi"));//wrong
+		System.out.println(multipleChoice.checkAnswer("1"));//wrong
+		System.out.println(multipleChoice.checkAnswer("3"));//right
+		String[] choices2 = {"yes", "no", "maybe", "I have no idea what's going on"};
+		Question multipleChoice2 = new MultipleChoiceQuestion("Do you like this quiz?", "yes", 5, choices2 );
+		multipleChoice2.displayPrompt();
 		System.out.println(multipleChoice.checkAnswer("hi"));//wrong
 		System.out.println(multipleChoice.checkAnswer("1"));//wrong
 		System.out.println(multipleChoice.checkAnswer("3"));//right
